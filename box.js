db2adabf54921c1f124873df855670818f4b4151
jhanvi <shahjhanvi898@gmail.com>
2020-12-01 19:18:27 +0530

    doing my supply mission

---
+++ box.js
@@ -0,0 +1,14 @@
+class Box {
+var options={
+    restitution:1;
+}
+this.Body=Bodies rectangle(200,100,50,50,options)
+world.add(world.this. body)
+
+display(){
+var pos=this body.position;
+rectMode(CENTER)
+fill (225);
+rect (pos.X,pos.y, this.Width,this.height)
+}
+}
